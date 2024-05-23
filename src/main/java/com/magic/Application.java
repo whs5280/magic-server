4427fd62d53a69c7721bebe6f04d0c13b8cec6aa
whs5280 <13434118350@163.com>
2024-05-23 15:14:08 +0800

    feat: load mapper

--- src/main/java/com/magic/Application.java
+++ src/main/java/com/magic/Application.java
@@ -1,8 +1,10 @@
 package com.magic;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.magic.module.order.mapper")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
