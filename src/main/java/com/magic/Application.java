e0eb9512af5e66f890a83c536c0f6339975d924d
whs5280 <13434118350@163.com>
2024-05-27 16:17:51 +0800

    feat: set timeZone

--- src/main/java/com/magic/Application.java
+++ src/main/java/com/magic/Application.java
@@ -3,11 +3,18 @@ package com.magic;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @MapperScan("com.magic.module.order.mapper")
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class Application {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication app = new SpringApplication(Application .class);
         app.run(args);
     }
